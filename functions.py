712148376af470248ced0696fff675b8580b3b62
ghulam46 <65816301+ghulam46@users.noreply.github.com>
2025-04-16 11:15:41 +0700

    Functions with multiple arguments

--- functions.py
+++ functions.py
@@ -1,8 +1,18 @@
-def get_pay(num_hours):
-    pay_pretax = num_hours * 15
-    pay_aftertax = pay_pretax * (1 - .12)
+# def get_pay(num_hours):
+#     pay_pretax = num_hours * 15
+#     pay_aftertax = pay_pretax * (1 - .12)
+#     return pay_aftertax
+
+# pay_fulltime = get_pay(40)
+# pay_parttime = get_pay(32)
+# print(pay_parttime)
+
+
+def get_pay(num_hours, hourly_rate, tax_rate):
+    pay_pretax = num_hours * hourly_rate
+    pay_aftertax = pay_pretax * (1 - tax_rate)
     return pay_aftertax
 
-pay_fulltime = get_pay(40)
-pay_parttime = get_pay(32)
-print(pay_parttime)
+pay_fulltime = get_pay(40, 15, .12)
+pay_parttime = get_pay(32, 15, .12)
+print(pay_fulltime)
